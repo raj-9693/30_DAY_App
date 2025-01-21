47ae89e7400f0a010db9e8bc3838b3f41af49ef9
raj-9693 <rajkumarnishad96930@gmail.com>
2025-01-21 17:52:35 +0530

    com

--- app/src/main/java/com/example/protej/a30_day/MainActivity2.kt
+++ app/src/main/java/com/example/protej/a30_day/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.protej.a30_day
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
@@ -16,14 +17,21 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         val button=findViewById<Button>(R.id.cearma)
+        val button1=findViewById<Button>(R.id.Broser)
 
         button.setOnClickListener {
 
-
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE) // Camera kholne ka action
             startActivity(cameraIntent) // Camera app ko launch karte hain
-
         }
+button1.setOnClickListener {
+
+    val intent=Intent(Intent.ACTION_VIEW)
+    intent.data= Uri.parse("https://github.com/raj-9693")
+    startActivity(intent)
+}
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
